24af9353391942bcf71b756e00e4de452452405d
jos-ren <josh.renema25@gmail.com>
2023-08-23 13:18:49 -0700

    connected tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -2,85 +2,127 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useState, useEffect } from "react";
-import { MoviesData, renderMovieTitle } from "./movies_data.js";
 // import Autocomplete from "react-autocomplete";
 
-// connect to movie api
-// add autocomplete
-// connect to database
+// search a movie
+// display a list of results
+// add the ones you wish to add to your list
+// optionally rate them?
 
 export default function Home() {
   const [name, setName] = useState("");
   const [movies, setMovies] = useState([]);
-  const [number, setNumber] = useState(0);
-  // console.log(process.env.NEXT_PUBLIC_TMDB_API_KEY)
+  const [list, setList] = useState([]);
+  const [search, setSearch] = useState([]);
+  const fetch = require("node-fetch");
+
+  // const url = "https://api.themoviedb.org/3/find/tt14998742?external_source=imdb_id";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer " + process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN,
+    },
+  };
 
   useEffect(() => {
     const localMovies = JSON.parse(localStorage.getItem("movies"));
-    const localNumber = JSON.parse(localStorage.getItem("number"));
     if (localMovies) {
       setMovies(localMovies);
     }
-    if (localNumber) {
-      setNumber(localNumber);
-    }
+
+    // fetch top movies
+    fetch("https://api.themoviedb.org/3/discover/movie?include_adult=true&include_video=false&language=en-US&page=1&sort_by=popularity.desc", options)
+      .then((res) => res.json())
+      .then((json) => setList(json))
+      .catch((err) => console.error("error:" + err));
+    // console.log("fetched", list);
   }, []);
 
   return (
     <>
-      <h2>Movie List</h2>
-      <p>movie data is stored in local storage</p>
+      <h1>Search</h1>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button
-        onClick={() => {         
-          setMovies([...movies, { id: number, name: name }]);
-          localStorage.setItem("movies", JSON.stringify([...movies, { id: number, name: name }]));
-          
-          // add 1 to global id
-          let newNum = number + 1
-          setNumber(newNum);
-          localStorage.setItem("number", JSON.stringify(newNum));
+        onClick={() => {
+          fetch("https://api.themoviedb.org/3/search/multi?&language=en-US&query=" + name + "&page=1&include_adult=true", options)
+            .then((res) => res.json())
+            .then((json) => setSearch(json))
+            .catch((err) => console.error("error:" + err));
+          console.log(search.results)
         }}
       >
-        Add Movie
+        Search Movies
       </button>
-      {/* <Autocomplete
-        // value={this.state.val}
-        items={MoviesData()}
-        // getItemValue={item => item.title}
-        // shouldItemRender={renderMovieTitle}
-        // renderMenu={item => (
-        //   <div className="dropdown">
-        //     {item}
-        //   </div>
-        // )}
-        // renderItem={(item, isHighlighted) =>
-        //   <div className={`item ${isHighlighted ? 'selected-item' : ''}`}>
-        //     {item.title}
-        //   </div>
-        // }
-        // onChange={(event, val) => this.setState({ val })}
-        // onSelect={val => this.setState({ val })}
-      /> */}
+      {search.results ? (
+        <ul>
+          {search.results.map((result) => (
+            // only show movies in search
+            result.media_type === "movie" ?
+              <li key={result.id}>
+                <div style={{
+                  display: "flex",
+                  width: "500px",
+                  justifyContent: "space-between",
+                  paddingBottom: "5px",
+                }}>
+                  {result.title}
+                  <button
+                    onClick={() => {
+                      setMovies([...movies, { id: result.id, name: result.title }]);
+                      localStorage.setItem("movies", JSON.stringify([...movies, { id: result.id, name: result.title }]));
+                    }}
+                  >
+                    Add to List
+                  </button>
+                </div>
+              </li>
+              : <></>
+          ))
+          }
+        </ul>
+      ) : (
+        <></>
+      )}
+
+      <h2>My Movies List</h2>
       <ul>
         {movies.map((movie) => (
           <li key={movie.id}>
-            <div style={{display:"flex", width:"200px", justifyContent:"space-between", paddingBottom:"5px"}}>
-
-            {movie.name}{" "}
-            <button
-              onClick={() => {
-                // console.log(movie.id)
-                setMovies(movies.filter((a) => a.id !== movie.id));
-                localStorage.setItem('movies', JSON.stringify(movies.filter((a) => a.id !== movie.id)));
+            <div
+              style={{
+                display: "flex",
+                width: "500px",
+                justifyContent: "space-between",
+                paddingBottom: "5px",
               }}
+            >
+              {movie.name}{" "}
+              <button
+                onClick={() => {
+                  // console.log(movie.id)
+                  setMovies(movies.filter((a) => a.id !== movie.id));
+                  localStorage.setItem("movies", JSON.stringify(movies.filter((a) => a.id !== movie.id)));
+                }}
               >
-              Delete
-            </button>
-              </div>
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
+
+      <br />
+      <h2>Current Top 20 Movies</h2>
+      {list.results ? (
+        <ol>
+          {list.results.map((o, i) => (
+            <li key={i}>{o.title}</li>
+          ))}
+        </ol>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
