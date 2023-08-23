0219ddc80c3a18b4fbba7f5ee1587a91ad47d0b7
jos-ren <josh.renema25@gmail.com>
2023-08-22 19:00:38 -0700

    updated form

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,87 @@
 "use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { MoviesData, renderMovieTitle } from "./movies_data.js";
+import Autocomplete from "react-autocomplete";
+
+// add autocomplete
+// delete
+// connect to database
+// connect to movie api
 
 export default function Home() {
   const [name, setName] = useState("");
   const [movies, setMovies] = useState([]);
+  const [number, setNumber] = useState(0);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(name)
-    movies.push(name)
-    console.log(movies)
-    // alert(`The name you entered was: ${name}`);
-  };
+  useEffect(() => {
+    const localMovies = JSON.parse(localStorage.getItem("movies"));
+    const localNumber = JSON.parse(localStorage.getItem("number"));
+    if (localMovies) {
+      setMovies(localMovies);
+    }
+    if (localNumber) {
+      setNumber(localNumber);
+    }
+  }, []);
 
   return (
-    <main>
-      <h1>Movies List</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter your name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <input type="submit" />
-      </form>
-      <br></br>
-      <br></br>
-      <div>Movies: </div>
-      {name}
-      <br/>
-      <br/>
-      {movies}
-      {movies.map(function(data) {
-      return (
-        <div>
-          - {data}
-        </div>
-      )
-    })}
-      {/* map movies here */}
-    </main>
+    <>
+      <h2>Movie List</h2>
+      <p>movie data is stored in local storage</p>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          
+          setMovies([...movies, { id: number, name: name }]);
+          localStorage.setItem("movies", JSON.stringify([...movies, { id: number, name: name }]));
+          
+          // add 1 to global id
+          let newNum = number + 1
+          setNumber(newNum);
+          localStorage.setItem("number", JSON.stringify(newNum));
+        }}
+      >
+        Add Movie
+      </button>
+      {/* <Autocomplete
+        // value={this.state.val}
+        items={MoviesData()}
+        // getItemValue={item => item.title}
+        // shouldItemRender={renderMovieTitle}
+        // renderMenu={item => (
+        //   <div className="dropdown">
+        //     {item}
+        //   </div>
+        // )}
+        // renderItem={(item, isHighlighted) =>
+        //   <div className={`item ${isHighlighted ? 'selected-item' : ''}`}>
+        //     {item.title}
+        //   </div>
+        // }
+        // onChange={(event, val) => this.setState({ val })}
+        // onSelect={val => this.setState({ val })}
+      /> */}
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <div style={{display:"flex", width:"200px", justifyContent:"space-between", paddingBottom:"5px"}}>
+
+            {movie.name}{" "}
+            <button
+              onClick={() => {
+                // console.log(movie.id)
+                setMovies(movies.filter((a) => a.id !== movie.id));
+                localStorage.setItem('movies', JSON.stringify(movies.filter((a) => a.id !== movie.id)));
+              }}
+              >
+              Delete
+            </button>
+              </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
