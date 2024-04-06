c3b50d3ea810a9e4c4d5ad2c6249b3890076586d
Josh Renema <60946895+jos-ren@users.noreply.github.com>
2024-04-05 21:22:38 -0700

    Refactor AddMoreMovies component

--- src/components/statistics/AddMoreMovies.js
+++ src/components/statistics/AddMoreMovies.js
@@ -11,12 +11,10 @@ const AddMoreMovies = ({ heading, text }) => {
     return (
         <Box>
             <div style={{ padding: '4px', display: "flex", gap: "12px", justifyContent: "center", alignItems: "center", width: '100%' }}>
-                <div style={{width:"20px", display:"flex", justifyContent:"center", alignItems:"center"}}>
-                <PiPulseBold />
+                <div style={{ width: "20px", display: "flex", justifyContent: "center", alignItems: "center" }}>
+                    <PiPulseBold />
                 </div>
-                <div>
-                    Looks like this space could use some life! Why not kickstart things by adding some more movies or shows to "Seen"?
-                </div>
+                <div>Looks like this space could use some life! Why not kickstart things by adding some more movies or shows to &quot;Seen&quot;?</div>
             </div>
         </Box>
     );
